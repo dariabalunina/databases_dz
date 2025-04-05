b5c8739ee5cbf1faeb5b3297b85d7f6deb379b45
Дарья Балунина <ti.dasha207@gmail.com>
2025-04-05 18:45:33 +0300

    Add dz2

---
+++ dz_2/Script-1.sql
@@ -0,0 +1,45 @@
+create table if not exists Collections(
+	id SERIAL primary key,
+	name VARCHAR(40) not null,
+	year numeric check (year > 0)
+); 
+
+create table if not exists Genres(
+	id SERIAL primary key,
+	name VARCHAR(40) not null
+);
+
+create table if not exists Authors(
+	id SERIAL primary key,
+	name VARCHAR(40) not null
+); 
+
+create table if not exists Albums(
+	id SERIAL primary key,
+	name VARCHAR(40) not null,
+	year numeric check (year > 0)
+); 
+
+create table if not exists Tracks(
+	id SERIAL primary key,
+	name VARCHAR(40) not null,
+	duration interval not null,
+	id_album integer references Albums(id)
+); 
+
+
+create table if not exists GenresAuthors(
+	genre_id integer references Genres(id),
+	author_id integer references Authors(id)
+);
+
+create table if not exists AlbumsAuthors(
+	album_id integer references Albums(id),
+	author_id integer references Authors(id)
+);
+
+create table if not exists CollectionsTracks(
+	collection_id integer references Collections(id),
+	track_id integer references Tracks(id)
+);
+
